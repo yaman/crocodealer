aa58386c4028a97b326726f2d54f3d5d0d7fbad7
canavar <abdulkadiryaman@gmail.com>
2019-06-18 17:44:36 +0300

    [#3] Basic configuration types and label rules

---
+++ src/Crocodealer/Config.hs
@@ -0,0 +1,25 @@
+module Crocodealer.Config
+  ( LabelRule
+  , Label
+  , Package
+  , Config
+  ) where
+
+data Label = Label
+  { name        :: String
+  , description :: String
+  , color       :: String -- Data.Colour.sRGB24read can convert string specifications like "#00aaff" or "00aaff" 
+  }
+
+data LabelRule
+  = Create
+  | Override
+
+type Package = String
+
+data Config = Config
+  { username        :: Maybe String
+  , repository      :: Maybe String
+  , labelRules           :: [LabelRule]
+  , ignoredPackages :: [Package]
+  }
